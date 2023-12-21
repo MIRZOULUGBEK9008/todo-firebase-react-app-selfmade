3d9f8fbec92059ba8d33ea347327794a5a652b29
mirzoulugbek9008 <ulugbekkhudoyberdiyev9008@gmail.com>
2023-12-21 11:26:04 +0500

    Add header and ThemeController

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import Loader from "./components/Loader";
+import Header from "./components/Header";
 
 function App() {
   const [loader, setLoader] = useState(false);
-  return loader && <Loader />;
+  return (
+    <>
+      {loader && <Loader />}
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+
+function Header() {
+  return (
+    <header className="shadow-sm">
+      <div className="my-container">
+        <Navbar />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
